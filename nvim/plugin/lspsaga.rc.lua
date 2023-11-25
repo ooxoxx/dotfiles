3dd145569a38f747e09763cfc6f304c0255fbc89
leo <lihaoran2011@gmail.com>
2023-11-25 13:28:55 +0800

    fix: lsp and lspsaga keybindings

--- nvim/plugin/lspsaga.rc.lua
+++ nvim/plugin/lspsaga.rc.lua
@@ -20,9 +20,10 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', 'gn', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_buf_diagnostics<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+vim.keymap.set('n', 'gi', '<Cmd>Lspsaga finder imp+def+tyd<CR>', opts)
+vim.keymap.set('n', 'gR', '<Cmd>Lspsaga finder ref<CR>', opts)
 -- vim.keymap.set('i', '<C-q>', '<Cmd>Lspsaga signature_help<CR>', opts)
-vim.keymap.set('i', '<C-g>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+vim.keymap.set('i', '<C-q>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'gt', '<Cmd>Lspsaga rename<CR>', opts)
 
